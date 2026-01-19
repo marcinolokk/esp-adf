168c1a1cfce8537297f356693e53fc10e2b0300a
Marcin <marcin.zajaczkowski95@gmail.com>
2026-01-19 18:57:19 +0100

    fix: tune the REC button voltages

--- components/audio_board/lyrat_mini_v1_1/board.c
+++ components/audio_board/lyrat_mini_v1_1/board.c
@@ -115,7 +115,7 @@ esp_err_t audio_board_key_init(esp_periph_set_handle_t set)
     periph_adc_button_cfg_t adc_btn_cfg = PERIPH_ADC_BUTTON_DEFAULT_CONFIG();
     adc_arr_t adc_btn_tag = ADC_DEFAULT_ARR();
     adc_btn_tag.total_steps = 7;
-    int btn_array[8] = {100, 300, 600, 1000, 1375, 1775, 2195, 3100};
+    int btn_array[8] = {100, 300, 600, 1000, 1375, 1775, 2050, 2900};
     adc_btn_tag.adc_level_step = btn_array;
     adc_btn_cfg.arr = &adc_btn_tag;
     adc_btn_cfg.arr_size = 1;
